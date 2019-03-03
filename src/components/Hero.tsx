6400a1642308f2d57d5c298f7577655eccc72cb8
Kristóf Poduszló <kripod@protonmail.com>
2019-03-03 12:22:42 +0100

    Rename image variables

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,15 +7,15 @@ import FullHeight from './FullHeight';
 import ParallaxLayer from './ParallaxLayer';
 import ParallaxWrapper from './ParallaxWrapper';
 
-import BGLayer01URL from '../assets/hero/01-background.svg';
-import BGLayer02URL from '../assets/hero/02-stripes.svg';
-import BGLayer03URL from '../assets/hero/03-stars.svg';
-import BGLayer04URL from '../assets/hero/04-planets.svg';
-import BGLayer05URL from '../assets/hero/05-flying-saucer.svg';
-import BGLayer06URL from '../assets/hero/06-ground.svg';
-import BGLayer07URL from '../assets/hero/07-rocks.svg';
-import BGLayer08URL from '../assets/hero/08-rocket.svg';
-import BGLayer09URL from '../assets/hero/09-thinking-monster.svg';
+import BackgroundURL from '../assets/hero/01-background.svg';
+import StripesURL from '../assets/hero/02-stripes.svg';
+import StarsURL from '../assets/hero/03-stars.svg';
+import PlanetsURL from '../assets/hero/04-planets.svg';
+import FlyingSaucerURL from '../assets/hero/05-flying-saucer.svg';
+import GroundURL from '../assets/hero/06-ground.svg';
+import RocksURL from '../assets/hero/07-rocks.svg';
+import RocketURL from '../assets/hero/08-rocket.svg';
+import ThinkingMonsterURL from '../assets/hero/09-thinking-monster.svg';
 
 type Props = {
   children: React.ReactNode;
@@ -62,25 +62,29 @@ export default function Hero({ children }: Props) {
 
   return (
     <ParallaxWrapper as={FullHeight}>
-      <ParallaxLayer src={BGLayer01URL} />
-      <ParallaxLayer src={BGLayer02URL} />
+      <ParallaxLayer src={BackgroundURL} />
+      <ParallaxLayer src={StripesURL} />
       <ParallaxLayer
-        src={BGLayer03URL}
+        src={StarsURL}
         translateX={-0.05 * offsetX}
         translateY={-0.05 * offsetY}
         offsetRange={0.1}
       />
       <ParallaxLayer
-        src={BGLayer04URL}
+        src={PlanetsURL}
         translateX={0.01 * offsetX}
         translateY={-0.01 * offsetY}
         offsetRange={0.02}
       />
-      <ParallaxLayer src={BGLayer05URL} position="50% 60%" size="15vmin" />
-      <ParallaxLayer src={BGLayer06URL} />
-      <ParallaxLayer src={BGLayer07URL} />
-      <ParallaxLayer src={BGLayer08URL} position="100% 100%" size="70vmin" />
-      <ParallaxLayer src={BGLayer09URL} position="20% 100%" size="30vmin" />
+      <ParallaxLayer src={FlyingSaucerURL} position="50% 60%" size="15vmin" />
+      <ParallaxLayer src={GroundURL} />
+      <ParallaxLayer src={RocksURL} />
+      <ParallaxLayer src={RocketURL} position="100% 100%" size="70vmin" />
+      <ParallaxLayer
+        src={ThinkingMonsterURL}
+        position="20% 100%"
+        size="30vmin"
+      />
 
       <HeroChildrenWrapper>{children}</HeroChildrenWrapper>
     </ParallaxWrapper>

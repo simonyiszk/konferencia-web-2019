b544e9fddf7000d48b82a54d5553ad80926f82ca
Kristóf Poduszló <kripod@protonmail.com>
2019-03-11 00:16:08 +0100

    Remove iOS Safari 100vh hack

--- src/components/FullHeight.tsx
+++ src/components/FullHeight.tsx
@@ -1,4 +1,3 @@
-import useWindowSize from '@rehooks/window-size';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -6,24 +5,8 @@ type Props = {
   children: React.ReactNode;
 };
 
-type BaseProps = {
-  minHeight?: string;
-};
-
-const FullHeightBase = styled.div<BaseProps>`
-  min-height: ${({ minHeight = '100vh' }) => minHeight};
+const FullHeight = styled.div<Props>`
+  min-height: 100vh;
 `;
 
-export default function FullHeight(props: Props) {
-  // Address iOS Safari viewport height bug
-  // TODO: Remove typeof check when https://github.com/rehooks/window-size/pull/7 gets merged
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const windowSize = typeof window !== 'undefined' && useWindowSize();
-
-  return (
-    <FullHeightBase
-      minHeight={windowSize ? `${windowSize.innerHeight}px` : undefined}
-      {...props}
-    />
-  );
-}
+export default FullHeight;

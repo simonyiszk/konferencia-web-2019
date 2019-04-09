80d3d7019f147a2c0806892df768067dc8847ebc
Kristóf Poduszló <kripod@protonmail.com>
2019-04-10 00:28:28 +0200

    Improve giveaway section appearance

--- src/components/Giveaway.tsx
+++ src/components/Giveaway.tsx
@@ -7,18 +7,18 @@ import { ReactComponent as DisplayGiveawayPromo } from '../assets/display-giveaw
 export default function Giveaway() {
   return (
     <>
-      <Measure maxWidth="16em" lineHeight={0} mx="auto">
+      <Measure as={Paragraph} textAlign="center" mx="auto">
+        Regisztrálj a konferenciára, látogass meg legalább 3&nbsp;standot a
+        helyszíni&nbsp;expón és <strong>nyerj egy ultrawide monitort!</strong>
+      </Measure>
+
+      <Measure maxWidth="16em" mx="auto" my="1.5em">
         <DisplayGiveawayPromo />
       </Measure>
 
-      <Note mt="1.75em">
-        Regisztrálj a konferenciára, látogass meg legalább 3 standot a helyszíni
-        expón és nyerj egy ultrawide monitort!
-      </Note>
-
-      <Paragraph fontSize={0} textAlign="center">
+      <Note>
         Egyes előadásokon további értékes nyereményekkel gazdagodhatsz!
-      </Paragraph>
+      </Note>
     </>
   );
 }

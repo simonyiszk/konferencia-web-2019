5385e69ba100c5bfca7c945e7c8fd919b570b847
Kristóf Poduszló <kripod@protonmail.com>
2019-04-10 02:33:49 +0200

    Fix Measure's type declaration

--- src/components/Measure.tsx
+++ src/components/Measure.tsx
@@ -5,7 +5,7 @@ type MeasureProps = TextProps & {
   maxWidth?: string | number;
 };
 
-const Measure = styled(Text)`
+const Measure = styled<MeasureProps>(Text)`
   max-width: ${({ maxWidth = '34em' }) => maxWidth};
 `;
 

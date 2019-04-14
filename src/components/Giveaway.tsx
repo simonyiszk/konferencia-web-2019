5711b1afe700ba66f8c0e2bc9fb6061712417eab
Kristóf Poduszló <kripod@protonmail.com>
2019-04-14 17:29:43 +0200

    Add a link to the expo's map

--- src/components/Giveaway.tsx
+++ src/components/Giveaway.tsx
@@ -3,13 +3,15 @@ import Measure from './Measure';
 import Note from './Note';
 import Paragraph from './Paragraph';
 import { ReactComponent as DisplayGiveawayPromo } from '../assets/display-giveaway-promo.svg';
+import ExpoMapURL from '../data/expo-map.pdf';
 
 export default function Giveaway() {
   return (
     <>
       <Measure as={Paragraph} textAlign="center" mx="auto">
         Regisztrálj a konferenciára, látogass meg legalább 3&nbsp;standot a
-        helyszíni&nbsp;expón és <strong>nyerj egy ultrawide monitort!</strong>
+        <a href={ExpoMapURL}>helyszíni&nbsp;expón</a> és{' '}
+        <strong>nyerj egy ultrawide monitort!</strong>
       </Measure>
 
       <Measure maxWidth="16em" mx="auto" my="1.5em">

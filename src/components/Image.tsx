f74c38f5f10a74b15961b4b105e1aaec7126d45a
Kristóf Poduszló <kripod@protonmail.com>
2019-04-16 14:10:25 +0200

    Resolve some TODOs

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,11 +2,7 @@ import { Box, BoxProps } from '@rebass/grid';
 import styled from 'styled-components';
 import { height, HeightProps } from 'styled-system';
 
-type ImageProps = BoxProps &
-  HeightProps &
-  JSX.IntrinsicElements['img'] & {
-    as?: React.ElementType; // TODO: Remove this line
-  };
+type ImageProps = BoxProps & HeightProps & JSX.IntrinsicElements['img'];
 
 const Image = styled(Box)<ImageProps>`
   ${height}

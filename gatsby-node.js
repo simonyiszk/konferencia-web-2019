d4350aa4774e976b87f48d7593898a2b4ec01a8a
Kristóf Poduszló <kripod@protonmail.com>
2019-05-04 02:42:11 +0200

    Implement absolute path image node attachment

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,3 +28,33 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+
+exports.onCreateNode = ({ node, getNodes, boundActionCreators }) => {
+  // Attach each picture's ImageSharp node by public path if necessary
+  if (
+    node.internal.type === 'ProgramsYaml' &&
+    typeof node.presenter.picture === 'string'
+  ) {
+    const { createParentChildLink } = boundActionCreators;
+
+    // Find absolute path of linked path
+    const pathToFile = path
+      .join(__dirname, 'static', node.presenter.picture)
+      .split(path.sep)
+      .join('/'); // TODO: Use `String.prototype.replaceAll` once available
+
+    // Find ID of File node
+    const fileNode = getNodes().find(n => n.absolutePath === pathToFile);
+
+    if (fileNode != null) {
+      // Find ImageSharp node corresponding to the File node
+      const imageSharpNodeId = fileNode.children.find(n =>
+        n.endsWith('>> ImageSharp'),
+      );
+      const imageSharpNode = getNodes().find(n => n.id === imageSharpNodeId);
+
+      // Add ImageSharp node as child
+      createParentChildLink({ parent: node, child: imageSharpNode });
+    }
+  }
+};
